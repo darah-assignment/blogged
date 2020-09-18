0554d209f4bd5d5e2c5fcd4b0a9d44373e03914b
Darah <darashalabi@gmail.com>
2020-09-18 21:45:33 +0300

    prjct setup and store

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          <main>
+            <Route exact path='/'>
+              <h1>Hello, you've been Blogged!</h1>
+            </Route>
+          </main>
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
